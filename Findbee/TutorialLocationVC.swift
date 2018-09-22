adbca75c06d5f5b1d306c883086019316ecca2ed
pilot <be2man@hotmail.com>
2018-09-22 12:26:36 -0700

    swift3.0 project

---
+++ Findbee/TutorialLocationVC.swift
@@ -0,0 +1,35 @@
+//
+//  TutorialLocationVC.swift
+//  Findbee
+//
+//  Created by justin on 3/17/18.
+//  Copyright © 2018 Hakan. All rights reserved.
+//
+
+import UIKit
+
+class TutorialLocationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-FindbeeUITests/Pods-FindbeeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FindbeeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FindbeeUITests
+@end
